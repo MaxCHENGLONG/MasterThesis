6c5ee87095f626a4af7f6b0d398ff4e665b73810
CHENGLONG LI <asuralmax0915@outlook.com>
2025-02-18 20:32:06 +0100

    add BinaryCNN3 for CIFAR10

--- Training/CNN1.py
+++ Training/CNN1.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-num_classes = 2  # 二分类任务
-
+num_classes1 = 2  # 二分类任务
+num_classes2 = 10  # 多分类任务
 class BinaryCNN1(nn.Module):
     def __init__(self):
         super().__init__()
@@ -15,7 +15,7 @@ class BinaryCNN1(nn.Module):
                                       
         # 分类网络
         self.fc1 = nn.Linear(1600, 64)          
-        self.fc2 = nn.Linear(64, num_classes)  # 修改输出层维度为 2
+        self.fc2 = nn.Linear(64, num_classes1)  # 修改输出层维度为 2
 
     # 前向传播
     def forward(self, x):
@@ -29,6 +29,37 @@ class BinaryCNN1(nn.Module):
 
         return x
 
+
+class BinaryCNN3(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 特征提取网络
+        # 修改输入通道数为3
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)  # 第一层卷积,卷积核大小为3*3
+        self.pool1 = nn.MaxPool2d(2)                  # 设置池化层，池化核大小为2*2
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3) # 第二层卷积,卷积核大小为3*3   
+        self.pool2 = nn.MaxPool2d(2) 
+                                      
+        # 分类网络
+        ## 输入：32×32 -> conv1: (32-3+1)=30 -> pool1: 30//2=15 -> conv2: (15-3+1)=13 -> pool2: 13//2=6
+        self.fc1 = nn.Linear(64 * 6 * 6, 64)          
+        self.fc2 = nn.Linear(64, num_classes1)  # 修改输出层维度为 2
+
+    # 前向传播
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))     
+        x = self.pool2(F.relu(self.conv2(x)))
+
+        x = torch.flatten(x, start_dim=1)
+
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)  # 直接输出 logits，不加激活函数
+
+        return x
+    
+
 if __name__ == '__main__':
-    model = BinaryCNN1()
-    print(model)
+    model1 = BinaryCNN1()
+    model2 = BinaryCNN3()
+    print(model1)
+    print(model2)
